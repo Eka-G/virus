7b594a989f2750e2320b003de7d129a4e12b688e
Eka-G <ekaglazkovaa@yandex.ru>
2020-11-04 03:00:17 +0300

    add interaction: menu, congation item, statistics slider

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$(".nav__menu--header>.nav__item").click(function() {
+		$(this).addClass("nav__item--active");
+		$(this).siblings("li").removeClass("nav__item--active");
+	});
+
+	$(".contagion__item-wrapper").click( function() {
+		$(this).addClass("contagion__item-wrapper--active");
+		$(this).siblings("div").removeClass("contagion__item-wrapper--active");
+	});
+
+	$("[data-slider]").slick({
+ 		infinite: false,
+ 		fade: false,
+ 		slidesToShow: 1,
+  		slidesToScroll: 1
+	});
+
+	
+});
