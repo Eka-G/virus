c549abfbab9ae6af44f02ccf92a56d7785c17c96
Eka-G <ekaglazkovaa@yandex.ru>
2020-11-05 17:16:01 +0300

    add: toggle menu

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {
+
+	// MENU
 	$(".nav__menu--header>.nav__item").click(function() {
 		$(this).addClass("nav__item--active");
-		$(this).siblings("li").removeClass("nav__item--active");
+		$(this).siblings().removeClass("nav__item--active");
 	});
 
+	$(".nav__menu-toggle").click(function() {
+		$(".nav__menu").slideToggle();
+	});
+
+
+	//
 	$(".contagion__item-wrapper").click( function() {
 		$(this).addClass("contagion__item-wrapper--active");
-		$(this).siblings("div").removeClass("contagion__item-wrapper--active");
+		$(this).siblings().removeClass("contagion__item-wrapper--active");
 	});
 
+
+	// SLIDER
 	$("[data-slider]").slick({
  		infinite: false,
  		fade: false,
@@ -22,5 +32,5 @@ $(document).ready(function() {
 		$(this).siblings().removeClass("slick-arrow--unavailable");
 		$(this).addClass("slick-arrow--unavailable");
 	});
-
+	//
 });
